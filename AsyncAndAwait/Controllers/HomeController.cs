e3aafc32ca3f6442cb1ba1150a70e397b605e5f1
Faisal Pathan <faisalmpathan@gmail.com>
2019-05-25 17:16:39 +0530

    Initial Commit

---
+++ AsyncAndAwait/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using AsyncAndAwait.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AsyncAndAwait.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        [HttpGet]
+        public ActionResult Index()
+        {
+            Stopwatch watch= new Stopwatch();
+            watch.Start();
+            DemoMethods methods = new DemoMethods();
+            var content = methods.Content();
+            var count = methods.Count();
+            var name = methods.Name();
+            watch.Stop();
+            ViewBag.WatchMilliseconds = watch.ElapsedMilliseconds;
+            return View();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> IndexAsync()
+        {
+            Stopwatch watch = new Stopwatch();
+            DemoMethods methods = new DemoMethods();
+            watch.Start();
+            var contentTask = methods.ContentAsync();
+            var countTask = methods.CountAsync();
+            var nameTask = methods.NameAsync();
+
+            var content = await contentTask;
+            var count = await countTask;
+            var name = await nameTask;
+            watch.Stop();
+            ViewBag.WatchMilliseconds = watch.ElapsedMilliseconds;
+            return View("Index");
+        }
+    }
+}
---
+++ AsyncAndAwait/Models/DemoMethods.cs
@@ -0,0 +1,43 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncAndAwait.Models
+{
+    public class DemoMethods
+    {
+        public string Content()
+        {
+            Thread.Sleep(3000);
+            return "Synchronous Method";
+        }
+
+        public int Count()
+        {
+            Thread.Sleep(6000);
+            return 10;
+        }
+
+        public string Name()
+        {
+            Thread.Sleep(4000);
+            return "Faisal Pathan";
+        }
+        public async Task<string> ContentAsync()
+        {
+            await Task.Delay(3000);
+            return "Asynchronous Method";
+        }
+
+        public async Task<int> CountAsync()
+        {
+            await Task.Delay(6000);
+            return 10;
+        }
+
+        public async Task<string> NameAsync()
+        {
+            await Task.Delay(4000);
+            return "Faisal Pathan";
+        }
+    }
+}
